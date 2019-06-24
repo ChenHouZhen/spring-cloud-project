143efaf996581bf725fd33827cd080b4e751ee8c
chenhouzhen <chenhouzhen@hqjy.com>
2019-06-24 15:56:23 +0800

    add module monitor and eureka

---
+++ project-eureka/src/main/java/com/chenhz/eureka/ProjectEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.chenhz.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectEurekaApplication.class, args);
+    }
+
+}
---
+++ project-monitor/src/main/java/com/chenhz/monitor/ProjectMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.chenhz.monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectMonitorApplication.class, args);
+    }
+
+}
