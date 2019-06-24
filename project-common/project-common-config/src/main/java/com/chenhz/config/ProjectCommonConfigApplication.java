c8d83d757229c79de0e1b6c693407ac49b978237
chenhouzhen <chenhouzhen@hqjy.com>
2019-06-24 15:17:35 +0800

    init

---
+++ project-common/project-common-config/src/main/java/com/chenhz/config/ProjectCommonConfigApplication.java
@@ -0,0 +1,13 @@
+package com.chenhz.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectCommonConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectCommonConfigApplication.class, args);
+    }
+
+}
