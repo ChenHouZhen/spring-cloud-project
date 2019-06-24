52f9075bdfbc75a1e8390133cceb2f3135d32eb6
chenhouzhen <chenhouzhen@hqjy.com>
2019-06-24 16:15:27 +0800

    add module uac and uac api

---
+++ project-modules/project-modules-uac/src/main/java/com/chenhz/uac/ProjectModulesUacApplication.java
@@ -0,0 +1,13 @@
+package com.chenhz.uac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectModulesUacApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectModulesUacApplication.class, args);
+    }
+
+}
