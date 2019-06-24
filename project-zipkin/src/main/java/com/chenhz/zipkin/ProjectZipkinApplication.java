4d4fd566565a68117c8a89141f4af384aed8b57d
chenhouzhen <chenhouzhen@hqjy.com>
2019-06-24 15:28:16 +0800

    zinkin + gateway

---
+++ project-zipkin/src/main/java/com/chenhz/zipkin/ProjectZipkinApplication.java
@@ -0,0 +1,13 @@
+package com.chenhz.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectZipkinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectZipkinApplication.class, args);
+    }
+
+}
