e587db88e093a71ea6aef01a6bc74583bc6ff9d5
chenhouzhen <chenhouzhen@hqjy.com>
2019-06-26 17:01:35 +0800

    配置

--- project-zipkin/src/main/java/com/chenhz/zipkin/ProjectZipkinApplication.java
+++ project-zipkin/src/main/java/com/chenhz/zipkin/ProjectZipkinApplication.java
@@ -2,7 +2,9 @@ package com.chenhz.zipkin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class ProjectZipkinApplication {
 
